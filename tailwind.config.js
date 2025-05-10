cbcefac6833b572affe0c252c0c6bd9e2b0badad
Hazim Bhat <103562501+Hammad-ctrl@users.noreply.github.com>
2025-05-10 15:30:50 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./*.html","./src/js/*.js"],
+  content: [
+    "./*.html",         
+    "./src/js/*.js"   
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          950: '#0B1120',  
+        },
+        green: {
+          950: '#0F291E', 
+        },
+      },
+    },
   },
+  safelist: [
+    'bg-blue-950',  
+    'bg-green-950', 
+    'bg-red-400',   
+  ],
   plugins: [],
-}
+};
